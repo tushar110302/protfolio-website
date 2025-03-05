da8bbbb0f4a8878e34fbd201f2ab237ca6066173
Tushar Ranjan <107484915+tushar110302@users.noreply.github.com>
2025-03-05 19:18:29 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -42,7 +42,7 @@ function Hero() {
                         animate={{x:0, opacity:1}}
                         transition={{duration: 0.5, delay:0.8}}
                         className='flex flex-col'>
-                        <a href='https://drive.google.com/file/d/1jcbM4HrP_1F1SDB4h13X8Z8CTLbmWUef/view?usp=sharing' target='_blank'>
+                        <a href='https://drive.google.com/file/d/1wG831LBWBHET-WhNTKTNScL4wPwHOLxO/view?usp=drive_link' target='_blank'>
                             <motion.button type="button" className="transition-all hover:scale-110 text-white bg-gradient-to-r from-purple-500 to-pink-500 focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-semibold rounded-full text-xl px-7 py-4 text-center me-2 inline-flex items-center ">
                                 <svg class="w-6 h-6 text-gray-800 dark:text-white me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                 <path fill-rule="evenodd" d="M9 2.221V7H4.221a2 2 0 0 1 .365-.5L8.5 2.586A2 2 0 0 1 9 2.22ZM11 2v5a2 2 0 0 1-2 2H4v11a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2h-7Z" clip-rule="evenodd"/></svg>
